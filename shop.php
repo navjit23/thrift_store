78144b85aa07a19097f4c426e7601e77cb7d312f
navjit23 <66906714+navjit23@users.noreply.github.com>
2023-04-11 23:49:49 -0500

    v1.8.3
    search pagination implemented
    only works with default search rn
    needs work with filters and categories

--- shop.php
+++ shop.php
@@ -3,39 +3,43 @@
 require('connect.php');
 
 
-
+// result end = rStart * whatever the no.of results
+//try saving search results in sessions
 
 // LOADING THE PAGE
-
+// for search pagination
+$result_start = (int)$_GET['result_start'];
+$no_of_results = 5 ;// make a dropdown or something so the user can change it later
 $categories1 = loading_categories();
 
 // check for GET value, if true category is filtered from here
-if($_GET){
+if(array_key_exists( 'category_id', $_GET) ){
     $categoryID = $_GET['category_id'];
 }
 
 // gets executed when user clicks search 
 if($_POST){
+    $result_start=0;
     $search_value = $_POST['searchText'];
     $sortBy = $_POST['sort_by'];
     $sortType = $_POST['sort_type'];
 
-    if(! $_GET){
+    if(! array_key_exists( 'category_id', $_GET)){
     $categoryID = $_POST['category'];
     }
 
     $row = search_bar_filter($search_value, $sortBy, $sortType, $categoryID);
 }
-
+//create an if else here with sessions
 // default page loadup with/without categories filtered
 else{
-    if($_GET){
-        $products = "SELECT * FROM products WHERE category_id = :category_id"; 
+    if(array_key_exists( 'category_id', $_GET)){
+        $products = "SELECT * FROM products WHERE category_id = :category_id "; 
         $statement = $db->prepare($products);
         $statement->bindValue(':category_id', $categoryID);
     }
     else{
-        $products = "SELECT * FROM products LIMIT 10";
+        $products = "SELECT * FROM products ";
         $statement = $db->prepare($products);
     }
     $statement->execute();
@@ -46,18 +50,26 @@ else{
     $row = $results;
 } 
 
+$no_of_pages = count($row) % $no_of_results ;
 
 
 
-
-// search Bar function takes 4 inputs to filter data and return the query result in an array
+/* search Bar function takes 4 inputs to filter data and return the query result in an array
+* @param $search_value contains the string that needs to be searched
+* @param $sortBy contains the value to order the query by
+* @param $sortType should be asc or desc
+* @param $category_id cotain the category_id to filter the query in WHERE clause
+*/
 function search_bar_filter($search_value, $sortBy, $sortType, $category_id){
     global $db;
     
-    $products = "SELECT * FROM products WHERE `name` LIKE '%$search_value%' AND category_id LIKE '$category_id' ORDER BY $sortBy $sortType ;";
+    $products = "SELECT * FROM products 
+        WHERE `name` LIKE '%$search_value%' AND category_id LIKE '$category_id' ORDER BY $sortBy $sortType   ;";
 
     if($category_id == "all_categories"){
-        $products = "SELECT * FROM products WHERE `name` LIKE '%$search_value%' ORDER BY $sortBy $sortType ;";
+        $products = "SELECT * FROM products 
+            WHERE `name` LIKE '%$search_value%' 
+            ORDER BY $sortBy $sortType" ;
     }
     $statement = $db->prepare($products);
     
@@ -127,7 +139,7 @@ function loading_categories(){
         </select>
 
         <!-- only loads the category dropdown if there is no category already selected -->
-        <?php if(!$_GET): ?>
+        <?php if(!array_key_exists( 'category_id', $_GET)): ?>
         <label for="category">Category</label> 
         <select name="category">
             <option value="all_categories"> ---All Categories--- </option>
@@ -143,9 +155,9 @@ function loading_categories(){
 
     <div id="category_bar">
         <ul>
-            <li><a href="shop.php">All categories</a></li>
+            <li><a href="shop.php?result_start=0">All categories</a></li>
             <?php foreach ($categories1 as $category) : ?>
-                <li> <a href="shop.php?category_id=<?= $category['category_id']?> "> <?= $category['category_name'] ?> </a> </li>
+                <li> <a href="shop.php?category_id=<?= $category['category_id']?>&result_start=0 "> <?= $category['category_name'] ?> </a> </li>
             <?php endforeach ?>
         </ul>
 
@@ -154,7 +166,8 @@ function loading_categories(){
     <div id="products">
 
     <?php if($row):
-    foreach ($row as $product): ?>
+    $products = array_slice($row, $result_start, $result_start+ $no_of_results);
+    foreach ($products as $product): ?>
 
     <div id= "product_div" style="border:solid 1px black; margin:5px">
          
@@ -174,9 +187,26 @@ function loading_categories(){
 
     else: ?>
     <h2> No Results Found</h2>
-    <a href="shop.php">Click here to view all products</a>
+    <a href="shop.php?result_start=0">Click here to view all products</a>
     <?php endif ?>
 
     </div>
+
+    <ul>
+    <?php if($row):
+
+        if($result_start>0){ ?>
+            <li><a href="shop.php?result_start=<?= $result_start - $no_of_results ?>"> <<< </a></li>
+        <?php } 
+
+        for ($page_number = 0; $page_number <=$no_of_pages; $page_number ++ ) :?>
+            <li><a href="shop.php?result_start=<?= $page_number * $no_of_results ?>"><?=$page_number ?></a></li>
+        <?php endfor ;
+
+        if($result_start< count($row)-$no_of_results ){ ?>
+            <li><a href="shop.php?result_start=<?= $result_start + $no_of_results ?>"> >>></a></li>
+        <?php } 
+    endif ?>
+    </ul>
 </body>
 </html>
